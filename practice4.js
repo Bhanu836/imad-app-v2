01147b4582f9d8d3d341bf1a7083ea0a10b62bc6
Bhanu <Bhanugyani@gmail.com>
2017-02-12 18:39:27 +0530

    Add files via upload

---
+++ practice4.js
@@ -0,0 +1,36 @@
+(function(){var app = angular.module("bookstore",[]);
+ 
+ app.controller("formCtrl",function(){
+ this.Aname= shabd;
+ this.entryno = 1;
+ this.place = function(){
+ this.Aname = review.name;
+ this.email= review.email;
+ this.bookname= review.bookname;
+ 
+ 
+ };
+ 
+ });
+ var shabd={
+ name :"bhanu",
+ class:"electrical"
+ };
+ app.controller("mycontroller",function(){
+ 
+ this.cinema = sochfilmki;
+ });
+ 		 
+		  app.controller("tabController",function(){
+				    this.numb = 1;
+					this.setTab= function(newValue){
+					this.numb= newValue;
+					};
+					this.isSet= function(valName){
+					  return this.numb === valName;
+					};
+					});
+
+ 
+ var sochfilmki =[{MOVIE_NAME:"HANGOVER - 1",REVIEW: "FAADU",STARS:"5"},{MOVIE_NAME: "HANGOVER - 2",REVIEW: "BAHUT FAADU",STARS:"5"},{MOVIE_NAME: "HANGOVER - 3",REVIEW:"MAATHA JHAJHKOR DIYA" ,STARS:"5"}]
+})();
