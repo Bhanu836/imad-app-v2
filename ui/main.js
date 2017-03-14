abe0789a8f7d2005ee84ff84af066297b76c0a26
Bhanu <Bhanugyani@gmail.com>
2017-03-15 01:38:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var submit = document.getElementById('submit_btn');
 submit.onClick = function(){
      var request = new XMLHttpRequest();
+     
      reuest.onreadystatechange = function(){
          if(request.readystate === XMLHttpRequest.DONE){
          
@@ -9,16 +10,18 @@ submit.onClick = function(){
              alert('logged in');
              
          }
-         else if (request.status === 403){
+         else if(request.status === 403){
              alert('wrong password');
          }
          
-         else(request.status === 500)
+         else if(request.status === 500)
          {
              alert('something went wrong');
          
          }
-     }};
+     }
+         
+     };
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value; 
 request.open('POST', 'http://bhanu836.imad.hasura-app.io/login',true);
