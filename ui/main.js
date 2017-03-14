34899b479c8406fbc9738ed11e3a786cc938c471
Bhanu <Bhanugyani@gmail.com>
2017-03-15 02:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,40 @@ submit.onClick = function(){
      };
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value; 
-  console.log(usrname);
+  console.log(username);
 request.open('POST', 'http://bhanu836.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username , password:password}));
+    
+    
+};
+var submit = document.getElementById('register_btn');
+submit.onClick = function(){
+     var request = new XMLHttpRequest();
+     
+     reuest.onreadystatechange = function(){
+         if(request.readystate === XMLHttpRequest.DONE){
+         
+         if(request.status === 200){
+             alert('registered');
+             
+         }
+         else if(request.status === 403){
+             alert('wrong password');
+         }
+         
+         else if(request.status === 500)
+         {
+             alert('something went wrong');
+         
+         }
+     }
+         
+     };
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value; 
+  console.log(username);
+request.open('POST', 'http://bhanu836.imad.hasura-app.io/create-user',true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username , password:password}));
     
