22ea3b43c0b525861bd7cb788517a0c92c2d14c8
Bhanu <Bhanugyani@gmail.com>
2017-03-18 03:44:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    console.log(username);
+    
      var request = new XMLHttpRequest();
      
      request.onreadystatechange = function(){
