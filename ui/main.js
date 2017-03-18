fbc1a0d730516701e03f245c241a218e3732e103
Bhanu <Bhanugyani@gmail.com>
2017-03-18 12:49:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ sub.onclick = function(){
     
      request.onreadystatechange = function(){
                  
-         if(request.readystate === XMLHttpRequest.DONE){
+         if(request.status === 200){
              alert("registered");
              
              regdiv.innerHTML =" registered";
