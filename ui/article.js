a9ec98277c783f443ab9f8d9dfa6b3361832a0c9
Bhanu <Bhanugyani@gmail.com>
2017-03-23 14:13:32 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -5,20 +5,20 @@ function loadCommentForm () {
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
-        <input type="submit" id="submit" value="Submit" />
+        <input type="submit" id="submit2" value="Submit" />
         <br/>
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
-    var submit = document.getElementById('submit');
-    submit.onclick = function () {
+    var submit2 = document.getElementById('submit');
+    submit2.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
         
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
+          
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
@@ -27,8 +27,8 @@ function loadCommentForm () {
                 } else {
                     alert('Error! Could not submit comment');
                 }
-                submit.value = 'Submit';
-          }
+                submit2.value = 'Submit';
+          
         };
         
         // Make the request
