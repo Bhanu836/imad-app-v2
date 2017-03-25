427686e99751a5e800620afe63172c9d4369d39d
Bhanu <Bhanugyani@gmail.com>
2017-03-25 20:54:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -221,6 +221,17 @@ app.get('/get-comments/:articleName', function (req, res) {
       }
    });
 });
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 app.post('/submit-comment/:articleName', function (req, res) {
    // Check if the user is logged in
    console.log('a');
