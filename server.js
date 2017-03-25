ae0c33c6fe65891461e6b2bdf0a2a44061c7c040
Bhanu <Bhanugyani@gmail.com>
2017-03-25 23:47:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -269,6 +269,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
        });     
     } else {
         res.status(403).send('Only logged in users can comment');
+        res.send("Only logged in users can comment");
     }
 });
 
